4d90de991bbc0cc71d9bb597504f7c1a7869c719
khant26 <khantmg262626@gmail.com>
2025-12-13 23:51:53 +0700

    Enable media file serving in production for PDF downloads

--- CMSproject/urls.py
+++ CMSproject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +26,14 @@ urlpatterns = [
     path('api/', include('chatbot.urls')),
 ]
 
-# Serve media files in both development and production
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in production (Render)
+# In production, we manually add the media URL pattern since static() only works in DEBUG mode
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
